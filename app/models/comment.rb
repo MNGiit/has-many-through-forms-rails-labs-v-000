3d78f0a9d8e35e5f44cd8362e2040509c3547f25
Mohammed Noori <kmanoorib@gmail.com>
2019-07-08 04:16:54 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user # impotant to pass comments_spec "creates a comment and a new user"
+  accepts_nested_attributes_for :user, reject_if: :all_blank # impotant to pass comments_spec "creates a comment and a new user"
 end
