bd5fe46e925cd5f01ec0960617e72f9ebb22dbac
Mohammed Noori <kmanoorib@gmail.com>
2019-07-08 00:30:16 +0000

    Automatically backed up by Learn

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,13 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
-  accepts_nested_attributes_for 
+  accepts_nested_attributes_for :categories
+  
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+  
 end
