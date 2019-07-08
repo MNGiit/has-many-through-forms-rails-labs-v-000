78e4795717ab23b4d8a0eab6b1727a48f24695f0
Mohammed Noori <kmanoorib@gmail.com>
2019-07-08 01:13:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,4 +16,11 @@ class Post < ActiveRecord::Base
     self.comments.uniq { |c| c.content }
   end
 
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comment_attributes.build(comment: comment)
+    end
+  end
+
 end
