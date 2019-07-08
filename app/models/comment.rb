5f85e19673790f2deaf32745ac218312a9c1806b
Mohammed Noori <kmanoorib@gmail.com>
2019-07-08 04:16:08 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user # impotant to pass comment
+  accepts_nested_attributes_for :user # impotant to pass comments_spec
 end
